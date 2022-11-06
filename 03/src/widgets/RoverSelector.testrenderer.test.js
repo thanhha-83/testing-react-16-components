0ce24bfa2d1031cdf98bdd392c7cdd09f9f23d26
ha.phan <ha.phan@9thwonder.com>
2022-11-06 23:24:38 +0700

    testing activation

--- 03/src/widgets/RoverSelector.testrenderer.test.js
+++ 03/src/widgets/RoverSelector.testrenderer.test.js
@@ -25,21 +25,80 @@ describe('RoverSelector', () => {
       })
       describe('none selected', () => {
         it('should select no rovers', () => {
-            const none = {spirit: false, opportunity: false, curiosity: false}
-            const tr = TestRenderer.create(
-              <RoverSelector
-                roversActive={none}
-                rovers={rovers}
-                roverSelection={none}
-                onRoverSelection={()=>{}}
-              />
-            )
-            const inputs = tr.root.findAllByProps({"data-testid": 'rover-selected'})
-            inputs.forEach((input) => {
-                expect(input.props.checked).toBe(false)
-            })
+          const none = {spirit: false, opportunity: false, curiosity: false}
+          const tr = TestRenderer.create(
+            <RoverSelector
+              roversActive={none}
+              rovers={rovers}
+              roverSelection={none}
+              onRoverSelection={()=>{}}
+            />
+          )
+          const inputs = tr.root.findAllByProps({"data-testid": 'rover-selected'})
+          inputs.forEach((input) => {
+            expect(input.props.checked).toBe(false)
+          })
         });
-    });
+      });
+    })
+    describe('activations', () => {
+      const inactiveCssClassExpression = /.*RoverSelector-inactive/
+      const roverDivPrefix = 'rover-div-';
+      describe('all active', () => {
+        it('should not have the inactive class', () => {
+          const all = {spirit: true, opportunity: true, curiosity: true}
+          const tr = TestRenderer.create(
+            <RoverSelector
+              roversActive={all}
+              rovers={rovers}
+              roverSelection={all}
+              onRoverSelection={()=>{}}
+            />
+          )
+          const divs = tr.root.findAll((instance) => (instance.props['data-testid'] || "").startsWith(roverDivPrefix))
+          divs.forEach((div) => {
+            expect(div.props.className).not.toMatch(inactiveCssClassExpression)
+          })
+        })
+      })
+      describe('all inactive', ()=>{
+        it('should have the inactive class', ()=>{
+          const all = {spirit: false, opportunity: false, curiosity: false}
+          const tr = TestRenderer.create(
+            <RoverSelector
+              roversActive={all}
+              rovers={rovers}
+              roverSelection={all}
+              onRoverSelection={()=>{}}
+            />
+          )
+          const divs = tr.root.findAll((instance) => (instance.props['data-testid'] || "").startsWith(roverDivPrefix))
+          divs.forEach((div) => {
+              expect(div.props.className).toMatch(inactiveCssClassExpression)
+          });
+        });
+      })
+      describe('mixed', ()=>{
+        it('should have the inactive class for inactive rovers', ()=>{
+          const activation = {spirit: false, opportunity: true, curiosity: true};
+          const tr = TestRenderer.create(
+            <RoverSelector
+              roversActive={activation}
+              rovers={rovers}
+              roverSelection={activation}
+              onRoverSelection={()=>{}}
+            />
+          )
+          Object.keys(activation).forEach((roverName) => {
+            const div = tr.root.findAll((instance) => (instance.props['data-testid'] || "") === (roverDivPrefix + roverName))[0]
+            if (activation[roverName]) {
+              expect(div.props.className).not.toMatch(inactiveCssClassExpression)
+            } else {
+              expect(div.props.className).toMatch(inactiveCssClassExpression)
+            }
+          });
+        });
+      });
     })
   })
 })
